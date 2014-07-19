f7b9580c3f6fa43e71cd88ab55a2a8aa1e2bc94e
samuelsingh <samuelsachin@hotmail.co.uk>
2014-07-19 20:06:39 +0100

    Update Second.java

--- MAC/src/Second.java
+++ MAC/src/Second.java
@@ -3,7 +3,7 @@ public class Second {
 
 	public static void main(String[] args) {
         System.out.println("second"); // Display the string.
-        
+        System.out.println("this is the chnage from github");
 	}
 
 }
