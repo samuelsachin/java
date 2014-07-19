95e79e28bd41bc70d242b270a0939607c7dc2684
samuelsingh <samuelsachin@hotmail.co.uk>
2014-07-19 15:55:53 +0100

    second commit

--- MAC/src/first.java
+++ MAC/src/first.java
@@ -3,6 +3,9 @@ public class first {
 
 	    public static void main(String[] args) {
 	        System.out.println("Hello World!"); // Display the string.
+	        System.out.println("more chnage done on mac");
+	    
+	    
 	    }
 	}
 	
